ccecfa826420fb1b2695d0a8d225f22e8bce2b83
haruHishiro <haru.aviv.vesna@gmail.co>
2022-05-04 23:24:33 +0300

    Добавил в типы сиволов новый тип

--- src/main/java/rune/TypeSymbol.java
+++ src/main/java/rune/TypeSymbol.java
@@ -3,7 +3,9 @@ package rune;
 public enum TypeSymbol {
     //example
     FORM_BALL,
-    FORM_CUBE
+    FORM_CUBE,
+
+    BIG_RUNE
     
     //задать типы всех символов у рун, независимо от их типа, без приоритета просто названия.
 }
