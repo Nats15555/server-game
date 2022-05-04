5e0091bce8f1784e6c8047ea6051bebf934f0f75
haruHishiro <haru.aviv.vesna@gmail.co>
2022-05-05 01:43:55 +0300

    Комменты. Мысли. Список. Написал комментарии, чтобы было чуть яснее, за что отвечает каждая руна. Написал пару предложений, как можно изменить архитектуру. Создал список, который хранит все существующие руны.
    По идее надо подумать над тем, как можно дать пользователю возможность редактировать любые параметры: размер, сила концентрации, количество итераций цикла.
    
    Можно попробовать начать выносить функционал в API сервера.

--- src/main/java/rune/Main.java
+++ src/main/java/rune/Main.java
@@ -6,12 +6,58 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Rune> smallfireball = new ArrayList<Rune>();
 
+        // данная последовательность рун создаёт огненный шар, который двигается вперед
+        // придаём форму сферы
         smallfireball.add(new Rune(10,1000, TypeSymbol.SPHERE_SHAPE_RUNE, TypeRune.SHAPE_RUNE));
+        // задаём размер
         smallfireball.add(new Rune(10,1000, TypeSymbol.SMALL_SIZE_RUNE, TypeRune.SIZE_RUNE));
+        // выбираем тип энергии
         smallfireball.add(new Rune(10,1000, TypeSymbol.FIRE_ENERGY_RUNE, TypeRune.ENERGY_TYPE_RUNE));
+        // выбираем уровень силы (мсщь)
         smallfireball.add(new Rune(10,1000, TypeSymbol.SMALL_CONCENTRATION_RUNE, TypeRune.CONCENTRATION_ENERGY_RUNE));
-        smallfireball.add(new Rune(10,1000, TypeSymbol.BASE_NOURISHING_RUNE, TypeRune.NOURISHING_RUNE));
-        smallfireball.add(new Rune(10,1000, TypeSymbol.BASE_CYCLE_RUNE, TypeRune.CYCLE_RUNE));
+        // Двигаем конструкцию вперед N раз
+        // Надо подумать над тем, как реализовать цикл.
+        // Либо добвать набор рун, что будут хранить в себе разное количество итераций
+        // Либо сделать возможность регулировать количество итераций клиентом.
+        smallfireball.add(new Rune(10,1000, TypeSymbol.START_CYCLE_RUNE, TypeRune.CYCLE_RUNE));
         smallfireball.add(new Rune(10,1000, TypeSymbol.FORWARD_DIRECTION_RUNE, TypeRune.DIRECTION_RUNE));
+        smallfireball.add(new Rune(10,1000, TypeSymbol.END_CYCLE_RUNE, TypeRune.CYCLE_RUNE));
+        // проверяем необходимое количество энергии на каст заклинания
+        smallfireball.add(new Rune(10,1000, TypeSymbol.BASE_NOURISHING_RUNE, TypeRune.NOURISHING_RUNE));
+        // необходимо продумать работу функции циклической руны
+
+        // список существующих рун
+        // над балансом можно подумать потом (затраты энергии и время каста)
+        ArrayList<Rune> runesList = new ArrayList<Rune>();
+        runesList.add(new Rune(0,200, TypeSymbol.BASE_NOURISHING_RUNE, TypeRune.NOURISHING_RUNE));
+
+        runesList.add(new Rune(10,200, TypeSymbol.SMALL_SIZE_RUNE, TypeRune.SIZE_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.MEDIUM_SIZE_RUNE, TypeRune.SIZE_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.LARGE_SIZE_RUNE, TypeRune.SIZE_RUNE));
+
+        runesList.add(new Rune(10,200, TypeSymbol.CUBE_SHAPE_RUNE, TypeRune.SHAPE_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.SPHERE_SHAPE_RUNE, TypeRune.SHAPE_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.WOLF_SHAPE_RUNE, TypeRune.SHAPE_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.TRIANGLE_SHAPE_RUNE, TypeRune.SHAPE_RUNE));
+
+        runesList.add(new Rune(10,200, TypeSymbol.SMALL_CONCENTRATION_RUNE, TypeRune.CONCENTRATION_ENERGY_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.MEDIUM_CONCENTRATION_RUNE, TypeRune.CONCENTRATION_ENERGY_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.BIG_CONCENTRATION_RUNE, TypeRune.CONCENTRATION_ENERGY_RUNE));
+
+        runesList.add(new Rune(10,200, TypeSymbol.FIRE_ENERGY_RUNE, TypeRune.ENERGY_TYPE_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.WATER_ENERGY_RUNE, TypeRune.ENERGY_TYPE_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.EARTH_ENERGY_RUNE, TypeRune.ENERGY_TYPE_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.AIR_ENERGY_RUNE, TypeRune.ENERGY_TYPE_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.PURE_ENERGY_RUNE, TypeRune.ENERGY_TYPE_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.PRANA_ENERGY_RUNE, TypeRune.ENERGY_TYPE_RUNE));
+
+        runesList.add(new Rune(10,200, TypeSymbol.START_CYCLE_RUNE, TypeRune.CYCLE_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.END_CYCLE_RUNE, TypeRune.CYCLE_RUNE));
+
+        runesList.add(new Rune(10,200, TypeSymbol.FORWARD_DIRECTION_RUNE, TypeRune.DIRECTION_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.BACKWARD_DIRECTION_RUNE, TypeRune.DIRECTION_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.LEFT_DIRECTION_RUNE, TypeRune.DIRECTION_RUNE));
+        runesList.add(new Rune(10,200, TypeSymbol.RIGHT_DIRECTION_RUNE, TypeRune.DIRECTION_RUNE));
+
     }
 }
--- src/main/java/rune/TypeSymbol.java
+++ src/main/java/rune/TypeSymbol.java
@@ -1,11 +1,14 @@
 package rune;
 
 public enum TypeSymbol {
+    //список всех существующих рун по группам
+
     //NOURISHING_RUNE
     BASE_NOURISHING_RUNE,
 
     //SIZE_RUNE
-    BIG_SIZE_RUNE,
+    // В идеале надо сделать полную настройку размера клиентом в заданнах нами границах
+    LARGE_SIZE_RUNE,
     MEDIUM_SIZE_RUNE,
     SMALL_SIZE_RUNE,
 
@@ -16,6 +19,9 @@ public enum TypeSymbol {
     WOLF_SHAPE_RUNE,
 
     //CONCENTRATION_ENERGY_RUNE
+    // Так же и здесь, надо сделать полную настройку концентрации клиентом в заданнах нами границах
+    // То есть например добавить для всех рун служебное значение, которое в зависимости от руны
+    // будет иметь тот или иной смысл.
     BIG_CONCENTRATION_RUNE,
     MEDIUM_CONCENTRATION_RUNE,
     SMALL_CONCENTRATION_RUNE,
@@ -29,13 +35,23 @@ public enum TypeSymbol {
     PRANA_ENERGY_RUNE,
 
     //CYCLE_RUNE
-    BASE_CYCLE_RUNE,
+    //можно заменить на одну руну, если ввести служебную переменную
+    START_CYCLE_RUNE,
+    END_CYCLE_RUNE,
 
     //DIRECTION_RUNE
     LEFT_DIRECTION_RUNE,
     RIGHT_DIRECTION_RUNE,
     FORWARD_DIRECTION_RUNE,
-    BACKWARD_DIRECTION_RUNE
-    
-    //задать типы всех символов у рун, независимо от их типа, без приоритета просто названия.
+    BACKWARD_DIRECTION_RUNE;
+
+    // Я бы добавил каждой руне поля: <стоимость в мане> и <количество затрачиваемового времени на каст>
+
+
+    // эта конструкция решит множество проблем, вопрос надо ли её добавлять или можно обойтись без неё
+    // например её можно использовать, как счётчик итераций цикла; уровня концентрации; размера;
+    // длины шага, для рун типа движения; множитель затрат того или иного типа энергии
+    // public int serviceVariable;
+    // TypeRune(int serviceVariable) { this.serviceVariable = serviceVariable; }
+
 }
