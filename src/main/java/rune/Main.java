5bb70fc12f42e694c924ad1bf96ffb0dcd2282c6
haruHishiro <haru.aviv.vesna@gmail.co>
2022-05-05 00:15:20 +0300

    Добавил новый тип руны. Довые новые типы символов на рунах. Сделал пример того, как приблизительно будет выглядеть заклинание огненного шара

--- src/main/java/rune/Main.java
+++ src/main/java/rune/Main.java
@@ -1,8 +1,17 @@
 package rune;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        Rune rune = new Rune(15,1000, TypeSymbol.FORM_BALL, TypeRune.SHAPE_RUNE);
+        ArrayList<Rune> smallfireball = new ArrayList<Rune>();
+
+        smallfireball.add(new Rune(10,1000, TypeSymbol.SPHERE_SHAPE_RUNE, TypeRune.SHAPE_RUNE));
+        smallfireball.add(new Rune(10,1000, TypeSymbol.SMALL_SIZE_RUNE, TypeRune.SIZE_RUNE));
+        smallfireball.add(new Rune(10,1000, TypeSymbol.FIRE_ENERGY_RUNE, TypeRune.ENERGY_TYPE_RUNE));
+        smallfireball.add(new Rune(10,1000, TypeSymbol.SMALL_CONCENTRATION_RUNE, TypeRune.CONCENTRATION_ENERGY_RUNE));
+        smallfireball.add(new Rune(10,1000, TypeSymbol.BASE_NOURISHING_RUNE, TypeRune.NOURISHING_RUNE));
+        smallfireball.add(new Rune(10,1000, TypeSymbol.BASE_CYCLE_RUNE, TypeRune.CYCLE_RUNE));
+        smallfireball.add(new Rune(10,1000, TypeSymbol.FORWARD_DIRECTION_RUNE, TypeRune.DIRECTION_RUNE));
     }
 }
--- src/main/java/rune/TypeRune.java
+++ src/main/java/rune/TypeRune.java
@@ -5,7 +5,8 @@ public enum TypeRune {
     SHAPE_RUNE(0), //руна форма
     SIZE_RUNE(0), //руна размера
     CONCENTRATION_ENERGY_RUNE(0), //руна концентрации энергии
-    SOURCE_ENERGY_RUNE(0), //руна источника энергии
+    ENERGY_TYPE_RUNE(0), //руна типа энергии
+    CYCLE_RUNE(1), //руна зацикливания. для работы необходимо указать количество повторений
     DIRECTION_RUNE(1); //руна направления движения заклинания
 
     public int priority;
